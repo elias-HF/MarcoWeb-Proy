daf9da4cc1809781486e3deb319e73ad550b5ae5
Gabriel-Ato <gabrielato@gmail.com>
2026-08-29 18:17:01 -0500

    Modificada la navbar de empleado a una sidebar

---
+++ javascript/cargarInterfacesEmpleado.js
@@ -0,0 +1,37 @@
+function cargarVista(pagina, elemento){
+    fetch(`/html/CompoEmpleado/${pagina}`).then(response => response.text()).then(data => {
+        // Insertar el HTML dentro del main abajo del navbar
+        document.getElementById("panelPrincipal").innerHTML = data;
+
+        // Quitar el 'active' de todos los enlaces
+        const enlaces = document.querySelectorAll("#sidebar .nav-link");
+        enlaces.forEach(enlace => {
+            enlace.classList.remove("active");
+            enlace.classList.add("text-white");
+        });
+
+        // Activar el enlace clickeado
+        elemento.classList.add("active");
+        elemento.classList.remove("text-white");
+    })
+    .catch(error => {
+        console.error("Error al cargar la vista", error)
+    });
+}
+
+// Cargar la interfaz de inicio automáticamente
+document.addEventListener("DOMContentLoaded", function () {
+    const inicio = document.querySelector("#sidebar .nav-link");
+    cargarVista("inicio.html", inicio);
+});
+
+function cambiarSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("collapsed");
+}
+
+// Función propia del java de bootstrap
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+    new bootstrap.Tooltip(tooltipTriggerEl);
+});
