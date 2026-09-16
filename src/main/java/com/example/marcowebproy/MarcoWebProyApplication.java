8eb29d033587b463a4e953fab055ef93a87ddb2d
eli-hu-24 <eliashuaringa244@gmail.com>
2026-09-16 18:49:10 -0500

    reestructuracion del proyecto a intellij

---
+++ src/main/java/com/example/marcowebproy/MarcoWebProyApplication.java
@@ -0,0 +1,13 @@
+package com.example.marcowebproy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcoWebProyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarcoWebProyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/marcowebproy/MarcoWebProyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marcowebproy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcoWebProyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
