7e98165e1447ceb54f7673717c3d8b000e6872ae
eli-hu-24 <eliashuaringa244@gmail.com>
2026-09-21 20:42:40 -0500

    se agregaron las carpetas para controler, services,model.

---
+++ src/main/java/com/example/marcowebproy/controller/FragmentController.java
@@ -0,0 +1,4 @@
+package com.example.marcowebproy.controller;
+
+public class FragmentController {
+}
---
+++ src/main/java/com/example/marcowebproy/controller/ThymeleafController.java
@@ -0,0 +1,4 @@
+package com.example.marcowebproy.controller;
+//aqui van los objetos y la info ya que no tenemos bd todavia..
+public class ThymeleafController {
+}
