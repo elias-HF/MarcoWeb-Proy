bcefe73f94e8e1d83d435ab65a107f03756573ee
juan880-art <fuentesjuancoz@gmail.com>
2026-09-24 13:09:56 -0500

    Add getters and setters to Categoria class

--- src/main/java/com/example/marcowebproy/model/Categoria.java
+++ src/main/java/com/example/marcowebproy/model/Categoria.java
@@ -16,4 +16,16 @@ public class Categoria {
         this.descripcion = descripcion;
         this.estado = estado;
     }
+    // Getters y Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+
+    public boolean isEstado() { return estado; }
+    public void setEstado(boolean estado) { this.estado = estado; }
 }
