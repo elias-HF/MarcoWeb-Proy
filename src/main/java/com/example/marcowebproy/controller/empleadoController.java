ea03b010d5a0e21748149f7925f9b057a98058d5
Lookvrt <looksvrt@gmail.com>
2026-09-25 20:59:34 -0500

    feat(categorias): implementar iteracion dinamica y simulacion de datos en memoria
    - Se instancian objetos Categoria y Recurso en @GetMapping para simulacion de datos.
    - Se adapta la vista HTML con Thymeleaf para el conteo dinamico y renderizado de badges/iconos Bootstrap.

--- src/main/java/com/example/marcowebproy/controller/empleadoController.java
+++ src/main/java/com/example/marcowebproy/controller/empleadoController.java
@@ -1,8 +1,14 @@
 package com.example.marcowebproy.controller;
 
+import com.example.marcowebproy.model.Categoria;
+import com.example.marcowebproy.model.Recurso;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/empleado")
@@ -15,7 +21,21 @@ public class empleadoController {
     public String dashboard() {return "compoEmpleado/dashboard";}
 
     @GetMapping("/categorias")
-    public String categorias() {
+    public String categorias(Model model) {
+        List<Categoria> categorias = new ArrayList<Categoria>();
+        /* Datos temporales creados dentro de @GetMapping para maquetación y renderizado dinámico con Thymeleaf/Bootstrap.
+        No se utilizo @PostMapping aún al no contar con el formulario de envío (Modal de registro)*/
+
+        Recurso r1 = new Recurso(101, "Laptop", null, "Disponible", "Buena");
+        Recurso r2 = new Recurso(102, "Proyector Epson", null, "Disponible", "Buena");
+
+        categorias.add(new Categoria(1, "Tecnología", "Equipos tecnológicos para uso académico.", r1, true));
+        categorias.add(new Categoria(2, "Audiovisual", "Equipos utilizados para presentaciones.", r2, true));
+        categorias.add(new Categoria(3, "Material académico", "Libros y materiales de consulta.", null, true));
+        categorias.add(new Categoria(4, "Accesorios", "Accesorios y complementos tecnológicos.", null, false));
+
+        model.addAttribute("categorias", categorias);
+
         return "compoEmpleado/categorias";
     }
 
--- src/main/java/com/example/marcowebproy/model/Categoria.java
+++ src/main/java/com/example/marcowebproy/model/Categoria.java
@@ -1,19 +1,25 @@
 package com.example.marcowebproy.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Categoria {
 
     private int id;
     private String nombre;
     private String descripcion;
+    private Recurso recursos;
     private boolean estado;
+
 
     public Categoria() {
     }
 
-    public Categoria(int id, String nombre, String descripcion, boolean estado) {
+    public Categoria(int id, String nombre, String descripcion, Recurso recursos, boolean estado) {
         this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
+        this.recursos = recursos;
         this.estado = estado;
     }
     // Getters y Setters
@@ -25,6 +31,9 @@ public class Categoria {
 
     public String getDescripcion() { return descripcion; }
     public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+
+    public Recurso getRecurso() { return recursos; }
+    public void setRecursos(Recurso recursos) {this.recursos = recursos;}
 
     public boolean isEstado() { return estado; }
     public void setEstado(boolean estado) { this.estado = estado; }
